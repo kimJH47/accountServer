e8f8c10ceaf6a20df9b30f8e571693c30ac6dafd
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 06:17:22 +0900

    refactor: 계좌생성 유효성검증 추가

--- src/main/java/core/accountserver/dto/request/CreateAccountRequest.java
+++ src/main/java/core/accountserver/dto/request/CreateAccountRequest.java
@@ -1,7 +1,7 @@
 package core.accountserver.dto.request;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,8 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateAccountRequest {
 
-	@NotNull(message = "아이디 값은 필수로 입력 되어야합니다.")
+	@NotNull(message = "아이디 값은 필수로 존재해야 합니다.")
+	@Min(value = 1, message = "아이디는 1 이상 이여야 합니다.")
 	private Long userId;
-	@Positive(message = "초기금액은 0 이상이여야 합니다.")
+	@NotNull(message = "초기금액은 필수로 존재해야 합니다.")
+	@Min(value = 100, message = "금액은 최소 100 이상 존재해야 합니다.")
 	private Long initialBalance;
 }
