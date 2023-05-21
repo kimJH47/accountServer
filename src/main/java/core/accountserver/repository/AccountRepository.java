82a4273e17ce60cbe3ef11dac488b8c596280398
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 07:39:09 +0900

    refactor: 생성된 계좌번호가 존재하는지 확인하는 메서드 구현

--- src/main/java/core/accountserver/repository/AccountRepository.java
+++ src/main/java/core/accountserver/repository/AccountRepository.java
@@ -1,7 +1,5 @@
 package core.accountserver.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import core.accountserver.domain.account.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
-	Optional<Account> findFirstByOrderByIdDesc();
+	boolean existsByAccountNumber(String accountNumber);
 }
