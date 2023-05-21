1695b2dcf73d7469fccc936a93818b93d16f412b
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 08:49:03 +0900

    feat: request 검증실패 exception handler 메서드 구현

---
+++ src/main/java/core/accountserver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package core.accountserver.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import core.accountserver.dto.response.Response;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Response> handle(MethodArgumentNotValidException e) {
+		return Response.createBadRequestResponse("잘못된 요청입니다", e);
+	}
+}
