23c941c269b847626c830222d9bcb8ede4426e54
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 06:17:51 +0900

    feat: user 가 존재하지 않을 시 발생하는 예외 구현

---
+++ src/main/java/core/accountserver/exception/user/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package core.accountserver.exception.user;
+
+public class UserNotFoundException extends RuntimeException {
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
