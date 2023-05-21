1e6fc0eae73cd7e495b74192a713f735aed1bb4c
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 08:00:51 +0900

    feat: 계좌생성 기능 구현

--- src/main/java/core/accountserver/service/AccountService.java
+++ src/main/java/core/accountserver/service/AccountService.java
@@ -1,8 +1,15 @@
 package core.accountserver.service;
 
-import org.springframework.stereotype.Service;
+import static core.accountserver.domain.account.AccountStatus.*;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import core.accountserver.domain.AccountUser;
+import core.accountserver.domain.account.Account;
+import core.accountserver.domain.account.AccountNumberGenerator;
 import core.accountserver.dto.response.CreateAccountResponse;
+import core.accountserver.exception.user.UserNotFoundException;
 import core.accountserver.repository.AccountRepository;
 import core.accountserver.repository.AccountUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +20,23 @@ public class AccountService {
 
 	private final AccountRepository accountRepository;
 	private final AccountUserRepository accountUserRepository;
+
+	private final AccountNumberGenerator accountNumberGenerator;
+
+	@Transactional
 	public CreateAccountResponse createAccount(Long userId, Long initialBalance) {
-		return null;
+		AccountUser accountUser = accountUserRepository.findById(userId)
+			.orElseThrow(() -> new UserNotFoundException("해당 사용자가 존재하지 않습니다."));
+
+		String createAccount = accountNumberGenerator.generator(userId);
+		while (accountRepository.existsByAccountNumber(createAccount)) {
+			createAccount = accountNumberGenerator.generator(userId);
+		}
+		Account account = accountRepository.save(Account.create(accountUser, createAccount, initialBalance, IN_USE));
+		return CreateAccountResponse.builder()
+			.userId(accountUser.getId())
+			.accountNumber(account.getAccountNumber())
+			.registeredAt(account.getRegisterAt())
+			.build();
 	}
 }
