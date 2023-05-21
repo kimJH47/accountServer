750f98892dacba67f8b70ed284c0f9be08a5cc16
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 06:40:55 +0900

    feat: account 마지막 id 조회 메서드 구현

--- src/main/java/core/accountserver/repository/AccountRepository.java
+++ src/main/java/core/accountserver/repository/AccountRepository.java
@@ -1,4 +1,13 @@
 package core.accountserver.repository;
 
-public interface AccountRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import core.accountserver.domain.account.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Long> {
+	Optional<Account> findFirstByOrderByIdDesc();
 }
