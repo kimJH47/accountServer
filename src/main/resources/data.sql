4370f3aec513f98eb553dc67bea2e1329d81ada6
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 08:34:25 +0900

    테스트 데이터 sql 파일 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+insert into account_user(id, name, create_at, update_at) values (1,'proro',now(),now());
+insert into account_user(id, name, create_at, update_at) values (2,'lupi',now(),now());
+insert into account_user(id, name, create_at, update_at) values (3,'eddie',now(),now());
