5bc0147dffeb7909013005e060872c1b17482e86
kimJaeHyeon <kmr2644@gmail.com>
2023-05-23 07:46:32 +0900

    feat: redis lock 서비스 구현

---
+++ src/main/java/core/accountserver/service/LockService.java
@@ -0,0 +1,41 @@
+package core.accountserver.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import core.accountserver.exception.transaction.TransactionHasLockException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LockService {
+
+	private final RedissonClient redissonClient;
+
+	public void lock(String accountNumber) {
+		RLock lock = redissonClient.getLock(getLockKey(accountNumber));
+		log.debug("Trying lock for accountNumber: {}", accountNumber);
+		try {
+			if (!lock.tryLock(1, 5, TimeUnit.SECONDS)) {
+				throw new TransactionHasLockException("해당 계좌는 사용중입니다.");
+			}
+		} catch (Exception e) {
+			log.error("Redis lock failed");
+		}
+	}
+
+	public void unlock(String accountNumber) {
+		log.debug("Trying unlock for accountNumber: {}",accountNumber);
+		redissonClient.getLock(getLockKey(accountNumber)).unlock();
+
+	}
+
+	private String getLockKey(String accountNumber) {
+		return "ACLK:" + accountNumber;
+	}
+}
