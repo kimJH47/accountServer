e58932359448be59e602a9f71c6c1a2cd235aeed
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 09:31:25 +0900

    feat: UserNotFoundException handler 메서드 구현

--- src/main/java/core/accountserver/exception/GlobalExceptionHandler.java
+++ src/main/java/core/accountserver/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import core.accountserver.dto.response.Response;
+import core.accountserver.exception.user.UserNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +14,10 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<Response> handle(MethodArgumentNotValidException e) {
 		return Response.createBadRequestResponse("잘못된 요청입니다.", e);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<Response> handle(UserNotFoundException e) {
+		return Response.createBadRequestResponse("잘못된 요청입니다.", "userId", e.getMessage());
 	}
 }
