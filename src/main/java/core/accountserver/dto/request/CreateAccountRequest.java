9dc8c31fb000be25b68cc8b31c51dd7d21aa7a22
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 09:21:19 +0900

    refactor: 필드 final 로 변경

--- src/main/java/core/accountserver/dto/request/CreateAccountRequest.java
+++ src/main/java/core/accountserver/dto/request/CreateAccountRequest.java
@@ -5,17 +5,15 @@ import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class CreateAccountRequest {
 
 	@NotNull(message = "아이디 값은 필수로 존재해야 합니다.")
 	@Min(value = 1, message = "아이디는 1 이상 이여야 합니다.")
-	private Long userId;
+	private final Long userId;
 	@NotNull(message = "초기금액은 필수로 존재해야 합니다.")
 	@Min(value = 100, message = "금액은 최소 100 이상 존재해야 합니다.")
-	private Long initialBalance;
+	private final Long initialBalance;
 }
