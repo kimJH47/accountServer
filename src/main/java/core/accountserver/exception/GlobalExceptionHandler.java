006831e5d5651c0af80efda2eb26141a30d291ea
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 09:55:43 +0900

    feat: MaxAccountPerUserException handler 메서드 구현

--- src/main/java/core/accountserver/exception/GlobalExceptionHandler.java
+++ src/main/java/core/accountserver/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import core.accountserver.dto.response.Response;
+import core.accountserver.exception.user.MaxAccountPerUserException;
 import core.accountserver.exception.user.UserNotFoundException;
 
 @ControllerAdvice
@@ -19,5 +20,10 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<Response> handle(UserNotFoundException e) {
 		return Response.createBadRequestResponse(BAD_REQUEST, "userId", e.getMessage());
+	}
+
+	@ExceptionHandler(MaxAccountPerUserException.class)
+	public ResponseEntity<Response> handle(MaxAccountPerUserException e) {
+		return Response.createBadRequestResponse(BAD_REQUEST, "account", e.getMessage());
 	}
 }
