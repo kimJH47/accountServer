e15b892e9702f3d6407efb2f4d2edb472be83df0
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 18:32:57 +0900

    feat: 계좌번호 조회 기능 구현

--- src/main/java/core/accountserver/repository/AccountRepository.java
+++ src/main/java/core/accountserver/repository/AccountRepository.java
@@ -1,5 +1,7 @@
 package core.accountserver.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ import core.accountserver.domain.account.Account;
 public interface AccountRepository extends JpaRepository<Account,Long> {
 	boolean existsByAccountNumber(String accountNumber);
 	Integer countByAccountUser(AccountUser accountUser);
+	Optional<Account> findByAccountNumber(String accountNumber);
 }
