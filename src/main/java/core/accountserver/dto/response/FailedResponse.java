9db78ad7daa8f050e091d1fa5e40623126b9a99a
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 09:21:03 +0900

    feat: getter 구현

--- src/main/java/core/accountserver/dto/response/FailedResponse.java
+++ src/main/java/core/accountserver/dto/response/FailedResponse.java
@@ -3,12 +3,17 @@ package core.accountserver.dto.response;
 import java.util.HashMap;
 
 public class FailedResponse extends Response {
-	private HashMap<String, String> reasons = new HashMap<>();
+	private final HashMap<String, String> reasons = new HashMap<>();
 	protected FailedResponse(String message) {
 		super(message);
 	}
 
 	public void input(String fieldName, String message) {
 		reasons.put(fieldName, message);
+		System.out.println(reasons);
+	}
+
+	public HashMap<String, String> getReasons() {
+		return reasons;
 	}
 }
