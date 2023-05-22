77fb5c8d092c7e2bb5210403571627ff7c22f198
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 20:04:39 +0900

    feat: accountUser 로 계좌조회 기능 구현

--- src/main/java/core/accountserver/repository/AccountUserRepository.java
+++ src/main/java/core/accountserver/repository/AccountUserRepository.java
@@ -1,11 +1,15 @@
 package core.accountserver.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import core.accountserver.domain.AccountUser;
+import core.accountserver.domain.account.Account;
 
 @Repository
 public interface AccountUserRepository extends JpaRepository<AccountUser,Long> {
 
+	List<Account> findByAccountUser(AccountUser accountUser);
 }
