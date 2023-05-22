e3ce0464e8e5630c07fff4f13a1e0532b4840d94
kimJaeHyeon <kmr2644@gmail.com>
2023-05-23 00:29:51 +0900

    feat :최대 거래취소 가능일자 상수 구현

---
+++ src/main/java/core/accountserver/policy/TransactionConstant.java
@@ -0,0 +1,10 @@
+package core.accountserver.policy;
+
+public class TransactionConstant {
+
+	public static final int MAX_TRANSACTION_CANCEL_YEARS_BOUND = 1;
+	private TransactionConstant() {
+
+	}
+
+}
