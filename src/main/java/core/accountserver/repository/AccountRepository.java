c3504e695ccf1e6e822319792c36a40530e1797f
kimJaeHyeon <kmr2644@gmail.com>
2023-05-22 09:51:04 +0900

    feat: 현재 생성되있는 계좌가 몇개인지 조회하는 메서드 구현

--- src/main/java/core/accountserver/repository/AccountRepository.java
+++ src/main/java/core/accountserver/repository/AccountRepository.java
@@ -3,9 +3,11 @@ package core.accountserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import core.accountserver.domain.AccountUser;
 import core.accountserver.domain.account.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
 	boolean existsByAccountNumber(String accountNumber);
+	Integer countByAccountUser(AccountUser accountUser);
 }
