8b76f1e03a740ed898e8f47fa22e8e51f9dddcb9
kimJaeHyeon <kmr2644@gmail.com>
2023-05-23 00:28:15 +0900

    feat: 트렌젝션 id 로 트렌젝션조회 기능 구현

--- src/main/java/core/accountserver/repository/TransactionRepository.java
+++ src/main/java/core/accountserver/repository/TransactionRepository.java
@@ -1,8 +1,12 @@
 package core.accountserver.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import core.accountserver.domain.transaction.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+	Optional<Transaction> findByTransactionId(String transactionId);
+
 }
